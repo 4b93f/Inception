281bc4113123245932c2b5e9095226482a2f53f5
4b93f <4b93f>
2022-11-07 23:15:12 +0100

    Everything is working fine, need to check how to log into database during foreground ...

--- requirements/nginx/tools/start.sh
+++ requirements/nginx/tools/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 openssl req -x509 -nodes -days 30 -subj "/C=FR/ST=PAYS/L=VILLE/O=42/OU=S19/CN=4b93f" -newkey rsa:4096 -keyout /etc/nginx/ssl/nginx-selfsigned.key -out /etc/nginx/ssl/nginx-selfsigned.crt;
-#service nginx restart
+# service nginx restart
 nginx -g "daemon off;"
--- requirements/wordpress/tools/start.sh
+++ requirements/wordpress/tools/start.sh
@@ -2,7 +2,7 @@
 wp plugin update --all --allow-root
 
 wp core download --allow-root
-wp core install --url=localhost --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
+wp core install --url=$WORDPRESS_URL --title=$WORDPRESS_TITLE --admin_user=$WORDPRESS_ADMIN_USER --admin_password=$WORDPRESS_ADMIN_PWD --admin_email=$WORDPRESS_ADMIN_EMAIL --allow-root
 wp user create $CORRECTOR_USER $CORRECTOR_USER_EMAIL --role=editor --user_pass=$CORRECTOR_USER_PWD --allow-root
 wp theme install twentynineteen --allow-root 
 wp theme activate twentynineteen --allow-root
