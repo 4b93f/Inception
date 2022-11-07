a7f0a1bb57f17717b3f32dcc6c22fad02e6da023
4b93f <4b93f>
2022-11-07 21:36:53 +0100

    pretty much done, but domain name doesnt work...

--- requirements/wordpress/tools/start.sh
+++ requirements/wordpress/tools/start.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 wp plugin update --all --allow-root
 
-wp user create $WORDPRESS_USER $WORDPRESS_USER_EMAIL --role=editor --user_pass=$WORDPRESS_USER_PWD --allow-root
+wp core download --allow-root
+wp core install --url=localhost --title=Example --admin_user=supervisor --admin_password=strongpassword --admin_email=info@example.com --allow-root
+wp user create $CORRECTOR_USER $CORRECTOR_USER_EMAIL --role=editor --user_pass=$CORRECTOR_USER_PWD --allow-root
+wp theme install twentynineteen --allow-root 
+wp theme activate twentynineteen --allow-root
 
 php-fpm7.3 --nodaemonize
